3e2db2395f5df35183e88baec47f7d0bf581a3ae
KevinBenigna <kevin.benigna@gmail.com>
2021-09-13 14:40:37 +0200

    v0.2

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
-let postittest = new PostIt(1, 200, 100, 400, 400, "#CCFFCC", "#0000FF", "Ceci est un test", "")
+window.addEventListener("load", () =>{
+
+let tablePostIt = [];
+
+tablePostIt.push(new PostIt(1, 7, 100, 150, 300, 300, "#CCFFCC", "#0000FF", "Ceci est un test", 24, 5, 1));
+tablePostIt.push(new PostIt(2, 2, 10, 300, 70, 70, "#FFCCCC", "#0000FF", "Ceci est encore un test", 12, -10, 1));
+
+function displayPostIt(){
+    for(let p = 0; p < tablePostIt.length; p++){
+        if(tablePostIt[p].status == 1){
+            tablePostIt[p].display();
+        }
+    }
+}
+displayPostIt();
+
+// Testing my classes
+
+tablePostIt[0].move(1, 200, 100);
+tablePostIt[0].resize(1, 50, 50);
+tablePostIt[0].forward(1, tablePostIt);
+console.log(tablePostIt[1].status);
+tablePostIt[1].sendToTrash();
+console.log(tablePostIt[1].status);
+
+
+})
--- js/post_it_class.js
+++ js/post_it_class.js
@@ -1,41 +1,84 @@
 class PostIt{
     id;
+    indexu;
     x;
     y;
     width;
-    heigh;
+    height;
     backgroundColor;
-    textColor;
+    color;
     text;
-    textSize;
+    fontSize;
     rotation;
+    status;
 
-    constructor(id, x, y, width, heigh, backgroundColor, textColor, text, texteSize, rotation){
+    constructor(id, indexu, x, y, width, height, backgroundColor, color, text, fontSize, rotation, status){
         this.id = id;
+        this.indexu = indexu;
         this.x = x;
         this.y = y;
         this.width = width;
-        this.heigh = heigh;
+        this.height = height;
         this.backgroundColor = backgroundColor;
-        this.textColor = textColor;
+        this.color = color;
         this.text = text;
-        this.textSize = texteSize;
+        this.fontSize = fontSize;
         this.rotation = rotation;
+        this.status = status;
     }
 
-    // display(){
-    //     let myPostit = document.createElement("div");
-    //     myPostit.
-    // }
-
-    move(){
-        
+    display(){
+        let myPostIt = document.createElement("div");
+        myPostIt.id = "PostIt_"+this.id;
+        myPostIt.classList.add("post_it");
+        myPostIt.style.zIndex = this.indexu;
+        myPostIt.style.left = this.x+"px";
+        myPostIt.style.top = this.y+"px";
+        myPostIt.style.width = this.width+"px";
+        myPostIt.style.height = this.height+"px";
+        myPostIt.style.backgroundColor = this.backgroundColor;
+        myPostIt.style.color = this.color;
+        myPostIt.innerHTML = this.text;
+        myPostIt.style.fontSize = this.fontSize+"px";
+        myPostIt.style.transform = "rotate("+this.rotation+"deg)";
+        myPostIt.style.position = "absolute";
+        document.getElementById("zone_post_it").appendChild(myPostIt);
     }
 
-    resize(){
-
+    move(id, moveX, moveY){
+        let myPostIt = document.getElementById("PostIt_"+id);
+        myPostIt.style.left = (this.x+moveX)+"px";
+        myPostIt.style.top = (this.y+moveY)+"px";
     }
 
+    resize(id, resizeWidth, resizeHeight){
+        let myPostIt = document.getElementById("PostIt_"+id);
+        myPostIt.style.width = (this.width+resizeWidth)+"px";
+        myPostIt.style.height = (this.height+resizeHeight)+"px";
+    }
+
+    // We check the entire tablePostIt to see the highest index and add 1 to it for our post-it
+    forward(id, tablePostIt){
+        let myPostIt = document.getElementById("PostIt_"+id);
+        let maxIndex = 0;
+        for(let p = 0; p < tablePostIt.length; p++){
+            let singlePostIt = document.getElementById("PostIt_"+tablePostIt[p].id);
+            if(singlePostIt.style.zIndex > maxIndex){
+                maxIndex = singlePostIt.style.zIndex;
+            }
+        }
+        myPostIt.style.zIndex = maxIndex++;
+    }
+
+    sendToTrash(){
+        this.status = 0;
+    }
+
+    // rotate a faire ou pas ?
+
+    changeBackgroundColor(){
+        this.
+    }
 
 
 }
